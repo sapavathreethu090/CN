ac0c60f39acf9bdff35f6bc4bd38f66004c1434e
sapavathreethu090 <sapavathreethu09@gmail.com>
2026-07-22 09:49:43 +0530

    Add files via upload

---
+++ bitdestuffing.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+	int ip_frame[100],op_frame[200];
+	int i,j=0,n;
+	int count=0;
+	printf("Enter frame lenth");
+	scanf("%d",&n);
+	printf("Enter input frame (0's and 1's only):\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&ip_frame[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		op_frame[j++]=ip_frame[i];
+		if(ip_frame[i]==1)
+		{
+		  count++;
+		if(count==5)
+		{
+		  i++;
+		  count=0;
+		 }
+		}
+		else
+		{
+		 count=0;
+		}
+	}
+	printf("\nAfter bit Stuffing the frame is :\n");
+	for(i=0;i<j;i++){
+		printf("%d",op_frame[i]);
+	}
+	printf("\n");
+}
